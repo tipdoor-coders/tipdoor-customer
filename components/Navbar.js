8dc38a92dbe7af35b389cbc0cd81e6837b038cb1
abhishal-ks <abhishalsharma2002@gmail.com>
2025-08-05 20:06:03 +0530

    minor fixes

--- components/Navbar.js
+++ components/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = () => {
     return (
         <header className='bg-white py-2.5 sticky top-0 z-50 md:shadow-sm shadow-md'>
             <nav className='flex justify-between items-center max-w-[1200px] mx-auto px-5 max-md:grid max-md:grid-cols-3 max-md:grid-rows-2'>
-                <div className="logo max-md:row-start-1 max-md:">
+                <div className="logo max-md:row-start-1">
                     <img className='md:h-12 h-10' src="/PNG.png" alt="Logo" />
                 </div>
 
@@ -64,14 +64,14 @@ const Navbar = () => {
 
                 <div className='flex items-center max-md:row-start-1'>
                     <input className='p-2 border-2 border-[#ccc] rounded-[4px] md:w-[250px] w-52 mr-2.5' type="text" placeholder="Search..." />
-                    <button className='px-3 py-1.5 bg-[rgba(95,24,235,0.4)] border-none rounded-[4px] text-black cursor-pointer hover:bg-[#5e17eb] hover:text-white' type="submit">Search</button>
+                    <button className='md:px-3 px-1.5 py-1.5 bg-[rgba(95,24,235,0.4)] border-none rounded-[4px] text-black cursor-pointer hover:bg-[#5e17eb] hover:text-white' type="submit">Search</button>
                 </div>
 
                 <div>
                     {session && <button className='text-white w-fit bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2' onClick={() => { signOut() }}>Logout</button>}
 
                     {!session && <Link href={"/login"}>
-                        <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2'>Login</button></Link>}
+                        <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm md:px-5 px-2.5 py-2.5 text-center me-2'>Login</button></Link>}
                 </div>
 
             </nav>
